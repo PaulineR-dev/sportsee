28b49e8724fac39a15b489dae4d534cad6dafb2a
Pauline Ramelet <pauline.ramelet@icloud.com>
2026-08-24 12:55:23 +0200

    Initial commit - SportSee project

---
+++ backend/mocks/mockData.js
@@ -0,0 +1,21 @@
+export const mockUserInfo = {
+  profile: {
+    firstName: "Sophie",
+    lastName: "Martin",
+    age: 31,
+    weight: 65,
+    height: 170,
+    profilePicture: "/images/sophie.png"
+  },
+  statistics: {
+    totalDistance: 120,
+    totalSessions: 18,
+    totalDuration: 540
+  }
+};
+
+export const mockActivity = [
+  { date: "2025-01-01", distance: 5.2, duration: 32 },
+  { date: "2025-01-03", distance: 6.1, duration: 38 },
+  { date: "2025-01-05", distance: 4.8, duration: 29 }
+];
