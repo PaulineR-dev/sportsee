037f421436534ae51b8efe46b0c88685f692c6ad
Pauline Ramelet <pauline.ramelet@icloud.com>
2026-08-26 12:57:19 +0200

    feat: create mockData file with user profiles and statistics (API structure)

---
+++ frontend/src/mocks/mockData.js
@@ -0,0 +1,54 @@
+// src/mocks/mockData.js
+
+export const mockData = {
+  sophie: {
+    profile: {
+      firstName: "Sophie",
+      lastName: "Martin",
+      createdAt: "2025-01-01",
+      age: 32,
+      weight: 60,
+      height: 165,
+      profilePicture: "http://localhost:8000/images/sophie.jpg"
+    },
+    statistics: {
+      totalDistance: 2250.2,
+      totalSessions: 348,
+      totalDuration: 14625
+    }
+  },
+
+  emma: {
+    profile: {
+      firstName: "Emma",
+      lastName: "Leroy",
+      createdAt: "2025-01-01",
+      age: 28,
+      weight: 62,
+      height: 170,
+      profilePicture: "http://localhost:8000/images/emma.jpg"
+    },
+    statistics: {
+      totalDistance: 3791.4,
+      totalSessions: 607,
+      totalDuration: 22624
+    }
+  },
+
+  marc: {
+    profile: {
+      firstName: "Marc",
+      lastName: "Dubois",
+      createdAt: "2025-01-01",
+      age: 45,
+      weight: 85,
+      height: 180,
+      profilePicture: "http://localhost:8000/images/marc.jpg"
+    },
+    statistics: {
+      totalDistance: 501.7,
+      totalSessions: 130,
+      totalDuration: 3555
+    }
+  }
+};
