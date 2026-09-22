4fc93ac8aecacfa735aa447305b9c64abdd5d739
Pauline Ramelet <pauline.ramelet@icloud.com>
2026-09-22 16:54:08 +0200

    chore: clear userId on logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -37,6 +37,7 @@ export default function Header() {
           id="logout-btn"
           onClick={() => {
             localStorage.removeItem("token");
+            localStorage.removeItem("userId");
             navigate("/");
           }}
         >
